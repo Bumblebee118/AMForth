4ecc2c24439319282c0c140f37f77cd849a31d18
azike <e11809909@student.tuwien.ac.at>
2021-03-28 22:22:37 +0200

    Created project in c lion

---
+++ cmake-build-debug/CMakeFiles/AMForth.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AMForth.exe"
+  "AMForth.exe.manifest"
+  "AMForth.pdb"
+  "CMakeFiles/AMForth.dir/main.c.obj"
+  "libAMForth.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AMForth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
