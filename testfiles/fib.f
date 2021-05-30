afea11bcd11d0ff79cf42c3362b3a0fee394a6d2
Alexander <42011733+azike@users.noreply.github.com>
2021-05-30 13:06:57 +0200

    edited the fib file

--- testfiles/fib.f
+++ testfiles/fib.f
@@ -1,4 +1,6 @@
 : 2dup 1 pick 1 pick ;
 variable count
 : resetCount 1 count ! ;
-: fib  resetCount 1 2 rot 0 do 2dup + rot count @ dup 1 + count ! . . cr loop drop drop ;
+: incrCount count @ dup 1 + count ! ;
+: printNum rot incrCount . . cr ;
+: fib resetCount 1 2 rot 0 do 2dup + printNum loop drop drop ;
