aceb170ce0629fe63e265af3fe6778a751180750
Alexander <42011733+azike@users.noreply.github.com>
2021-06-10 18:58:54 +0200

    changed the testfiles ending to .fs

--- testfiles/benchmark.sh
+++ testfiles/benchmark.sh
@@ -1,7 +1,7 @@
 for (( i = 0; i < 5; i++ )); do
-  #time ../cmake-build-release/AMForth < gcd.f > output.txt
-  #time gforth < gcd.f > output.txt
+  #time ../cmake-build-release/AMForth < gcd.fs > output.txt
+  #time gforth < gcd.fs > output.txt
 
-  time ../cmake-build-release/AMForth < fib_rec.f > output.txt
-  time gforth fib_rec_gforth.f > output.txt
+  time ../cmake-build-release/AMForth < fib_rec.fs > output.txt
+  time gforth fib_rec_gforth.fs > output.txt
 done
